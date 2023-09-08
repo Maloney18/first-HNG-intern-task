63579c9967a87aa52b7b730599bd7f86a81e60b4
Maloney18 <lekansolomon18@gmail.com>
2023-09-08 23:13:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const currentTime = document.querySelector(".current-time")
+const currentDay = document.querySelector(".current-day")
+
+// console.log(currentDay.innerHTML = new Date().getDay())
+// console.log(Date.UTC())
+
+const time = () => {
+    const dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const dayLight = new Date().getHours() <= 12 ? 'AM' : 'PM'
+    const hours = new Date().getHours() > 12 ? new Date().getHours() % 12 : new Date().getHours()
+    const minutes = new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}`: new Date().getMinutes()
+
+    currentDay.innerHTML = dayOfTheWeek[new Date().getDay()]
+    currentTime.innerHTML = `${hours} : ${minutes} ${dayLight}`
+}
+
+const navigate = () => {
+    window.open(`https://github.com/Maloney18`)
+}
+
+time()
