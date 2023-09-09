e1aa14d8016aa27db3b8030c7242062ca5339978
Maloney18 <lekansolomon18@gmail.com>
2023-09-09 23:35:06 +0100

    latest update

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const currentTime = document.querySelector(".current-time")
-const currentDay = document.querySelector(".current-day")
+const currentTime = document.querySelector("[data-testId='currentTime']")
+const currentDay = document.querySelector("[data-testId='currentDayOfTheWeek']")
 
-const time = () => {
-    const dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const dayLight = new Date().getHours() <= 12 ? 'AM' : 'PM'
-    const hours = new Date().getHours() > 12 ? new Date().getHours() % 12 : new Date().getHours()
-    const minutes = new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}`: new Date().getMinutes()
+const dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var dayLight = new Date().getHours() <= 12 ? 'AM' : 'PM'
+var hours = new Date().getHours() > 12 ? new Date().getHours() % 12 : new Date().getHours()
+var minutes = new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}`: new Date().getMinutes()
+
+currentDay.innerHTML = dayOfTheWeek[new Date().getDay()]
+currentTime.innerHTML = `${hours === 0 ? '12' : hours} : ${minutes} ${dayLight}`
+
+setInterval(() => {
+    dayLight = new Date().getHours() <= 12 ? 'AM' : 'PM'
+    hours = new Date().getHours() > 12 ? new Date().getHours() % 12 : new Date().getHours()
+    minutes = new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}`: new Date().getMinutes()
 
     currentDay.innerHTML = dayOfTheWeek[new Date().getDay()]
     currentTime.innerHTML = `${hours === 0 ? '12' : hours} : ${minutes} ${dayLight}`
-}
+}, 60000);
 
 const navigate = () => {
     window.open(`https://github.com/Maloney18/first-HNG-intern-task.git`)
 }
-
-time()
